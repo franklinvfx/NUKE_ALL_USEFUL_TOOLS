bfde55dd6fb2e29bff14893fa31af6522396261c
franklinvfx <42968358+franklinvfx@users.noreply.github.com>
2018-09-12 13:20:12 +0200

    3de

--- F_Scripts.py
+++ F_Scripts.py
@@ -46,27 +46,6 @@ except:   # Pixelfudger Don't exist
 
 
 #-----------------------------------------------------------------------------------------------------------------
-# 3DEQUALIZER
-#-----------------------------------------------------------------------------------------------------------------
-try:      # 3DE Exist
-	nuke.menu("Nodes").addMenu("3DE4", icon = "3de.png")
-	nuke.menu("Nodes").addCommand("3DE4/LD_3DE4_Anamorphic_Standard_Degree_4", "nuke.createNode('LD_3DE4_Anamorphic_Standard_Degree_4')")
-	nuke.menu("Nodes").addCommand("3DE4/LD_3DE4_Anamorphic_Rescaled_Degree_4", "nuke.createNode('LD_3DE4_Anamorphic_Rescaled_Degree_4')")
-	nuke.menu("Nodes").addCommand("3DE4/LD_3DE4_Anamorphic_Degree_6", "nuke.createNode('LD_3DE4_Anamorphic_Degree_6')")
-	nuke.menu("Nodes").addCommand("3DE4/LD_3DE4_Radial_Standard_Degree_4", "nuke.createNode('LD_3DE4_Radial_Standard_Degree_4')")
-	nuke.menu("Nodes").addCommand("3DE4/LD_3DE4_Radial_Fisheye_Degree_8", "nuke.createNode('LD_3DE4_Radial_Fisheye_Degree_8')")
-	nuke.menu("Nodes").addCommand("3DE4/LD_3DE_Classic_LD_Model", "nuke.createNode('LD_3DE_Classic_LD_Model')")
-	3DE1 = '- 3DE ........................... OK'
-	nuke.tprint(3DE1)
-except:   # 3DE Don't exist
-	3DE2 = '- 3DE ........................... FALSE'
-	nuke.tprint(3DE2)
-	##############################           #
-	pass
-
-
-
-#-----------------------------------------------------------------------------------------------------------------
 # OTHE TOOLS
 #-----------------------------------------------------------------------------------------------------------------
 try:      # ALL Exist

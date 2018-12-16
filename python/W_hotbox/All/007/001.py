bfb43e8138d25df3603954413780c3fdcce18e6b
Franklin <franklin.toussaint@gmail.com>
2018-12-16 18:12:30 +0100

    Delete 001.py

--- python/W_hotbox/All/007/001.py
+++
@@ -1,9 +0,0 @@
-#----------------------------------------------------------------------------------------------------------
-#
-# AUTOMATICALLY GENERATED FILE TO BE USED BY W_HOTBOX
-#
-# NAME: Count Selection
-#
-#----------------------------------------------------------------------------------------------------------
-
-nuke.message('%s / %s nodes selected'%(len(nuke.selectedNodes()),len(nuke.allNodes())))
